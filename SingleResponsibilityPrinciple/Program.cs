ced206a076818b20d03390bccd7e996754a0d2f1
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-05-22 21:01:51 +0600

    Add project files.

---
+++ SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static System.Console;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int Add(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // momento 
+        }
+
+        public void Remove(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, journal.ToString());
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.Add("I cried tpday");
+            j.Add("I ate a bug");
+
+            WriteLine(j);
+
+            var p = new Persistence();
+            var fileName = @"c:\temp\journal.txt";
+
+            p.SaveToFile(j, fileName, true);
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo(fileName)
+                {
+                    UseShellExecute = true
+                }
+            };
+
+            process.Start();
+        }
+    }
+}
