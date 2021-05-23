a498f4fd74fd090ab98730aa7fb688384d5940ff
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-05-23 15:34:28 +0600

    FacetedBuilder

---
+++ FacetedBuilder/PersonBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace FacetedBuilder
+{
+    public class PersonBuilder // facade
+    {
+        // reference
+        protected Person person = new Person();
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+        public static implicit operator Person(PersonBuilder pb)
+        {
+            return pb.person;
+        }
+    }
+}
---
+++ FacetedBuilder/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace FacetedBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+            Person person = pb
+                .Lives.At("London road 123")
+                      .In("London")
+                      .WithPostcode("sw1234")
+                .Works.At("Fabrikam")
+                      .AsA("Engineer")
+                      .Earning(12342);
+            WriteLine(person);
+        }
+    }
+}
