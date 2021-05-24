92f814c9c70ff2d60c20697d59255571faac3318
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-05-24 23:59:13 +0600

    AsyncFactoryMethod

---
+++ AsyncFactoryMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AsyncFactoryMethod
+{
+    class Foo
+    {
+        private Foo()
+        {
+        }
+
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsync();
+        }
+    }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var x = await Foo.CreateAsync();
+
+        }
+    }
+}
