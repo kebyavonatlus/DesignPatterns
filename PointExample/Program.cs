e6270a163cd96302a76a82b7d64faba0bb10b60b
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-05-24 23:49:58 +0600

    PointExample

---
+++ PointExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace PointExample
+{
+    internal enum CoordinateSystem
+    {
+        Cartesian,
+        Polar
+    }
+
+    public class Point
+    {
+        // factory method
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        private readonly double x;
+        private readonly double y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = Point.NewPolarPoint(1.0, Math.PI / 2);
+            WriteLine(point);
+        }
+    }
+}
